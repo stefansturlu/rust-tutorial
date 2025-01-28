5f0c61f25a2aba15a0f004c429cdb717d5dfb06b
Stefán Páll Sturluson <stefansturlu@gmail.com>
2025-01-28 23:41:18 +0000

    Chapter 3.1 - Varialbles and Mutability

---
+++ shadowing/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let x = 5;
+    println!("The value of x initially is: {x}");
+
+    let x = x + 1;
+    println!("The incremented value of x is: {x}");
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
