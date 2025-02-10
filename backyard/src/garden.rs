b7a5dfb8b36fdc81335df1a34ae4bd06d61495e8
Stefán Páll Sturluson <stefansturlu@gmail.com>
2025-02-10 23:33:17 +0000

    Chapter 7.2-7.3 - modules and module tree and paths

---
+++ backyard/src/garden.rs
@@ -0,0 +1,2 @@
+// This line means the code in src/garden/vegetables.rs is included too. That code is:
+pub mod vegetables;
---
+++ restaurant/src/lib.rs
@@ -0,0 +1,78 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+// eat_at_restaurant and front_of_house are siblings, so front_of_house doesn't need pub
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+}
+
+/*
+If you plan on sharing your library crate so other projects can use your code,
+your public API is your contract with users of your crate that determines how 
+they can interact with your code. 
+There are many considerations around managing changes to your public API to 
+make it easier for people to depend on your crate. These considerations are 
+out of the scope of this book; if you’re interested in this topic, see The 
+Rust API Guidelines (https://rust-lang.github.io/api-guidelines/).
+*/
