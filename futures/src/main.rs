fd91c73ee277648e57515cfb93d38d85b7b3dad1
Stefán Páll Sturluson <stefansturlu@gmail.com>
2025-04-07 23:31:16 +0000

    Chapter 17.1 - Futures and Async

---
+++ futures/src/main.rs
@@ -0,0 +1,49 @@
+use trpl::{Html,Either};
+use std::future::Future;
+
+async fn page_title(url: &str) -> (&str, Option<String>) {
+    // let response = trpl::get(url).await;
+    // let response_text = response.text().await;
+    let response_text = trpl::get(url).await.text().await;
+
+    let title = Html::parse(&response_text)
+        .select_first("title")
+        .map(|title_element| title_element.inner_html());
+    
+    (url, title)
+}
+
+// These two are equivalent (apart from tuple returned above)
+fn page_title_equivalent(url: &str) -> impl Future<Output = Option<String>> + '_ {
+    async move {
+        let text = trpl::get(url).await.text().await;
+        Html::parse(&text)
+            .select_first("title")
+            .map(|title| title.inner_html())
+    }
+}
+
+fn main() {
+    // Could also do trpl::run(page_title(url))
+    let example = trpl::run(page_title("http://www.rust-lang.org"));
+    println!("Example: {example:?}");
+    
+    let args: Vec<String> = std::env::args().collect();
+
+    trpl::run(async {
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title(&args[2]);
+
+        let (url, maybe_title) =
+            match trpl::race(title_fut_1, title_fut_2).await {
+                Either::Left(left) => left,
+                Either::Right(right) => right,
+            };
+
+        println!("{url} returned first");
+        match maybe_title {
+            Some(title) => println!("Its page title is: '{title}'"),
+            None => println!("Its title could not be parsed."),
+        }
+    })
+}
