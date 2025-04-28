3287a4dc40d9613617876ede016511c3072e20da
Stefán Páll Sturluson <stefansturlu@gmail.com>
2025-04-28 22:13:16 +0000

    Chapter 17.3 - Any number of futures

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,5 +1,11 @@
 use std::time::Duration;
-use trpl;
+use trpl::{self, Either};
+use std::{
+    pin::{pin,Pin},
+    future::Future,
+    thread,
+    time::Instant
+};
 
 fn main_1() {
     trpl::run(async {
@@ -76,14 +82,14 @@ fn question_3() -> i32 {
     0
 }
 
-fn main() {
+fn main_4() {
     trpl::run(async {
         let (tx, mut rx) = trpl::channel();
 
         let tx1 = tx.clone();
         // Note: the move keyword moves tx into the async block, which is dropped at end, 
         //   allowing the other async block to finish
-        let tx1_fut = async move {
+        let tx1_fut = pin!(async move {
             let vals = vec![
                 String::from("hi"),
                 String::from("from"),
@@ -95,15 +101,15 @@ fn main() {
                 tx1.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        };
+        });
 
-        let rx_fut = async {
+        let rx_fut = pin!(async {
             while let Some(value) = rx.recv().await {
                 println!("received '{value}'");
             }
-        };
+        });
 
-        let tx_fut = async move {
+        let tx_fut = pin!(async move {
             let vals = vec![
                 String::from("more"),
                 String::from("messages"),
@@ -115,8 +121,145 @@ fn main() {
                 tx.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(1500)).await;
             }
+        });
+        // You can use a macro instead of join, join2, join3, ...
+        // trpl::join!(tx1_fut, tx_fut, rx_fut);
+        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> =
+            vec![tx1_fut, rx_fut, tx_fut];
+        trpl::join_all(futures).await;
+    });
+}
+
+fn main_5() {
+    trpl::run(
+        async {
+            let a = async { 1u32 };
+            let b = async { "Hello!" };
+            let c = async { true };
+            
+            let (a_result, b_result, c_result) = trpl::join!(a, b, c);
+            println!("{a_result}, {b_result}, {c_result}");
+
+        }
+    )
+}
+
+fn main_6() {
+    trpl::run( async {
+        let slow = async {
+            println!("'slow' started.");
+            trpl::sleep(Duration::from_millis(100)).await;
+            println!("'slow' finished.");
         };
 
-        trpl::join3(tx1_fut, tx_fut, rx_fut).await;
+        let fast = async {
+            println!("'fast' started.");
+            trpl::sleep(Duration::from_millis(50)).await;
+            println!("'fast' finished.");
+        };
+
+        trpl::race(slow, fast).await;
+
+    })
+}
+
+fn slow(name: &str, ms: u64) {
+    thread::sleep(Duration::from_millis(ms));
+    println!("'{name}' ran for {ms}ms")
+}
+fn main_7() {
+    // Yieldinng
+    trpl::run(async {
+
+        let a = async {
+            println!("'a' started.");
+            trpl::yield_now().await;
+            slow("a", 30);
+            trpl::yield_now().await;
+            slow("a", 10);
+            trpl::yield_now().await;
+            slow("a", 20);
+            trpl::yield_now().await;
+            println!("'a' finished.");
+        };
+
+        let b = async {
+            println!("'b' started.");
+            trpl::yield_now().await;
+            slow("b", 75);
+            trpl::yield_now().await;
+            slow("b", 10);
+            trpl::yield_now().await;
+            slow("b", 15);
+            trpl::yield_now().await;
+            slow("b", 350);
+            trpl::yield_now().await;
+            println!("'b' finished.");
+        };
+
+        trpl::race(a, b).await;
+
     });
+}
+
+fn main_8() {
+    trpl::run(async {
+        let one_ns = Duration::from_nanos(1);
+        let start = Instant::now();
+        async {
+            for _ in 1..1000 {
+                trpl::sleep(one_ns).await;
+            }
+        }
+        .await;
+        let time = Instant::now() - start;
+        println!(
+            "'sleep' version finished after {} seconds.",
+            time.as_secs_f32()
+        );
+
+        let start = Instant::now();
+        async {
+            for _ in 1..1000 {
+                trpl::yield_now().await;
+            }
+        }
+        .await;
+        let time = Instant::now() - start;
+        println!(
+            "'yield' version finished after {} seconds.",
+            time.as_secs_f32()
+        );
+
+    });
+}
+
+async fn timeout<F: Future>(
+    future_to_try: F,
+    max_time: Duration
+) -> Result<F::Output, Duration> {
+    match trpl::race(future_to_try, trpl::sleep(max_time)).await {
+        Either::Left(output) => Ok(output),
+        Either::Right(_) => Err(max_time),
+    }
+}
+
+fn main_9() {
+    trpl::run(async {
+        let slow = async {
+            trpl::sleep(Duration::from_secs(5)).await;
+            "I finished!"
+        };
+
+        match timeout(slow, Duration::from_secs(2)).await {
+            Ok(message) => println!("Succeeded with '{message}'"),
+            Err(duration) => {
+                println!("Failed after {} seconds", duration.as_secs())
+            }
+        }
+    })
+}
+
+fn main() {
+    main_9()
 }
